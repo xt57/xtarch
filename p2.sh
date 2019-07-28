39378c1a1457d005c7ed5c6afcaca20c890a764d
Michael Williams <xt57dev@gmail.com>
2019-07-28 16:17:14 -0500

    commit p2.sh -m correcting syntax of --noconfirm

--- p2.sh
+++ p2.sh
@@ -29,7 +29,7 @@ log=./log
 for pkg in	xorg-server	xorg-xinit	i3-gaps		lightdm
 	do
 	print "installing $pkg"			>>	$log
-	pacman -Sy --no-confirm		$pkg	>>	$log 2>&1
+	pacman -Sy --noconfirm		$pkg	>>	$log 2>&1
 done
 
 #systemctl	start	lightdm.service
