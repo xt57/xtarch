337c96b21548d9166f0cf1d7d23617d90bf8d31a
Michael Williams <xt57dev@gmail.com>
2019-07-29 12:25:00 -0500

    commit p2.sh -m trimmed p2.sh to minimal scope

--- p2.sh
+++ p2.sh
@@ -17,23 +17,4 @@ pacman -Sy grub
 grub-install		/dev/sda
 grub-mkconfig	-o	/boot/grub/grub.cfg
 
-#	add user buildup here
-
-#	pacman	-S	sudo
-
-#	i3 research below this point
-
-log=./log
-> $log
-
-for pkg in	xorg-server	xorg-xinit	i3-gaps		dmenu
-	do
-	echo									|	tee -a $log
-	echo "installing $pkg"					|	tee -a $log
-	echo									|	tee -a $log
-	pacman -S --noconfirm	$pkg	2>&1 	|	tee -a $log
-done
-
-echo "be sure to setup /home/ag20253/.xinit"
-
 echo "xt57 p2 completed"
