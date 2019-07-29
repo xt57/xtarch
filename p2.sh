50ee0b0557bb366d0752f5a993da869e0a3ed939
Michael Williams <xt57dev@gmail.com>
2019-07-28 23:53:10 -0500

    commit p2.sh -m working from Luke's notes

--- p2.sh
+++ p2.sh
@@ -19,7 +19,7 @@ grub-mkconfig	-o	/boot/grub/grub.cfg
 
 #	add user buildup here
 
-pacman	-Sy	sudo
+#	pacman	-S	sudo
 
 #	i3 research below this point
 
@@ -27,13 +27,15 @@ log=./log
 > $log
 
 #for pkg in	xorg-server	xorg-xinit	i3-gaps		lightdm
-for pkg in	xorg-server	xorg-xinit	i3-gaps		lightdm
+for pkg in	xorg-server	xorg-xinit	i3-gaps		dmenu
 	do
 	echo "installing $pkg"			>>	$log
-	pacman -Sy --noconfirm		$pkg	>>	$log 2>&1
+	pacman -S --noconfirm	$pkg	>>	$log 2>&1
 done
 
 #systemctl	start	lightdm.service
 #systemctl	enable	lightdm.service
+
+echo "be sure to setup /home/ag20253/.xinit"
 
 echo "xt57 p2 completed"
