50bde842cf22e5c1a81250b93f19c13b067fe445
Michael Williams <xt57dev@gmail.com>
2019-07-30 12:57:36 -0500

    commit p2.sh -m stacked the passwd entry sessions

--- p2.sh
+++ p2.sh
@@ -10,6 +10,11 @@ echo "LANG=en_US.UTF-8"			>	$pre/etc/locale.conf
 echo "set root password"
 passwd
 
+# add users and their attributes here
+useradd	-m	-g wheel	ag20253
+echo "set user password"
+passwd					ag20253
+
 systemctl	start	dhcpcd
 systemctl	enable	dhcpcd
 
@@ -17,9 +22,5 @@ pacman -Sy --noconfirm	grub
 
 grub-install		/dev/sda
 grub-mkconfig	-o	/boot/grub/grub.cfg
-
-# add users and their attributes here
-useradd	-m	-g wheel	ag20253
-passwd					ag20253
 
 echo "xt57 p2 completed"
